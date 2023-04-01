3717481d724fd2b44da4dab8bfd0a39413b8ff83
unknown <gogapopp@gmail.com>
2023-04-01 22:02:26 +0300

    second increment is completed

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandler(t *testing.T) {
+	testCases := []struct {
+		urlPath      string
+		method       string
+		expectedCode int
+		contentType  string
+		expectedBody string
+	}{
+		{method: http.MethodPost, urlPath: "/", expectedCode: http.StatusCreated, expectedBody: "http://localhost:8080/EwHXdJfB"},
+		{method: http.MethodGet, urlPath: "/EwHXdJfB", expectedCode: http.StatusTemporaryRedirect, expectedBody: ""},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.method, func(t *testing.T) {
+			r := httptest.NewRequest(tc.method, tc.urlPath, nil)
+			w := httptest.NewRecorder()
+
+			mainHandler(w, r)
+
+			assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+			if tc.expectedBody != "" {
+				assert.Equal(t, tc.expectedBody, w.Body.String())
+			}
+		})
+	}
+}
