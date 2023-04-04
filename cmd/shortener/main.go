ad53dbfe10d7fed0ce71574a037ea5f0c5e747e9
unknown <gogapopp@gmail.com>
2023-04-04 21:38:42 +0300

    iter5 is completed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/gogapopp/shortener/config"
@@ -13,6 +14,13 @@ var BaseAddr string
 
 func main() {
 	flags := config.ParseFlags()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		flags.FlagRunAddr = envRunAddr
+	}
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		flags.FlagBaseAddr = envBaseAddr
+	}
 
 	// передаём FlagBaseAddr в handlers.go (функция записывает значение в переменную которая находится в пакете handlers)
 	BaseAddr := flags.FlagBaseAddr
